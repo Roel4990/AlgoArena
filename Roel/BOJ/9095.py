d8106a458858745064fef42a14c22646ab6daacd
roel <ash49901@gmail.com>
2026-03-25 20:27:08 +0900

    [BOJ 9095] 1,2,3 더하기 - Roel 코드 수정 - dp(1,2,3) 값 미리저장 - dp 저장을 안하고 있었음.

--- Roel/BOJ/9095.py
+++ Roel/BOJ/9095.py
@@ -1,22 +1,16 @@
 import sys
-
 input = sys.stdin.readline
 
 T = int(input())
 dp = [0] * 11
-
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
 def f(n):
     if dp[n]:
         return dp[n]
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 2
-    elif n == 3:
-        return 4
-    return f(n - 1) + f(n - 2) + f(n - 3)
-
-
+    dp[n] = f(n - 1) + f(n - 2) + f(n - 3)
+    return dp[n]
 for _ in range(T):
     n = int(input())
     print(f(n))
