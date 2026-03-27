ca04e1199d9aeb71f0a025b0b13402680838b242
khj011219 <khj011219@naver.com>
2026-03-27 15:56:46 +0900

    [BOJ 11727]2xn타일링2 - hyeonjun

---
+++ hyeonjun/BOJ/11727.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+answer = [0, 1]
+
+for i in range(2, n+1):
+    if i%2 == 0:
+        answer.append(answer[i-1]*2 + 1)
+
+    else:
+        answer.append(answer[i-1]*2 - 1)
+
+print(answer[n]%100007)
